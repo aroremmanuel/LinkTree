82ed1f6ba353f3e39558e0a54d9202b2f8778883
Aror Emmanuel <emmaaror@gmail.com>
2022-10-28 03:14:49 +0100

    Links added (second commit)

--- src/components/link.js
+++ src/components/link.js
@@ -1,7 +1,12 @@
 function Link() {
     return (
-        <div>
-            <h1>Link Component</h1>
+        <div id="link__container">
+            <a href="https://twitter.com/lactoseutd" target="blank" id="twitter"  className="link__button">Lactoseutd</a>
+            <a href="https://training.zuri.team/" target="blank" id="btn__zuri" className="link__button">Zuri Team</a>
+            <a href=" http://books.zuri.team" target="blank" id="books" className="link__button">Zuri Books</a>
+            <a href="https://books.zuri.team/python-for-beginners?ref_id=<ajebo>" target="blank" id="book__python" className="link__button">Python Books</a>
+            <a href="https://background.zuri.team" target="blank" id="pitch" className="link__button">Background Check for Coders</a>
+            <a href="https://books.zuri.team/design-rules" target="blank" id="book__design" className="link__button">Design Books</a>
         </div>
     );
 }
--- src/components/profile.js
+++ src/components/profile.js
@@ -3,10 +3,9 @@ import ProfilePic from "../media/profile-pic.jpg"
 function Profile() {
     return (
         <div>
-            <img src={ProfilePic} id="profile__img" />
-            <h1 id="profile__name">John Smith</h1>
-                <h1 id="twitter">lactoseutd</h1>
-                <h1 id="slack">nsdfd</h1>
+                <img src={ProfilePic} id="profile__img" />
+                <h1 id="profile__name">Aror Emmanuel</h1>
+                <h1 id="slack">ajebo</h1>
         </div>
     );
 }
