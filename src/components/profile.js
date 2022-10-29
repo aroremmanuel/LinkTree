f32ba870f88ae7b2364500d4ce89166466deb8ea
Odafe <odafe41@gmail.com>
2022-10-29 23:54:31 +0100

    Alt text updated

--- src/components/profile.js
+++ src/components/profile.js
@@ -3,7 +3,7 @@ import ProfilePic from "../media/profile-pic.jpg"
 function Profile() {
     return (
         <div>
-                <img src={ProfilePic} id="profile__img" />
+                <img src={ProfilePic} id="profile__img" alt="profile" />
                 <h1 id="profile__name">Aror Emmanuel</h1>
                 <h1 id="slack">ajebo</h1>
         </div>
