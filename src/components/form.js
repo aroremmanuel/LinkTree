64f54cd5c6bd6cb4a4a0ed4538aa8538dd64c4b0
Odafe <odafe41@gmail.com>
2022-11-05 13:30:18 +0100

    feat: Styled the contact button and did a few bug fixes

--- src/components/form.js
+++ src/components/form.js
@@ -5,7 +5,7 @@ function Form() {
             <p id="contact__subtext">Hi there, contact me to ask me about anything you have in mind.</p>
                 <form >
                     <div id="contact__form">
-                        <div id="name_container">
+                        <div id="name_container" className="input__container">
                             <div className="form_entry , name__field__container">
                                 <label for="first_name">First Name</label>
                                 <input type="text" placeholder="First Name" id="first_name" className="contact__input , name__field" required />
@@ -15,19 +15,19 @@ function Form() {
                                 <input type="text" placeholder="Last Name" id="last_name" className="contact__input , name__field" required />
                             </div>
                         </div>
-                        <div className="form_entry">
+                        <div className="form_entry , input__container">
                             <label for="email">Email</label>
                             <input type="text" placeholder="yourname@email.com" id="email" className="contact__input" required  />
                         </div>
-                        <div className="form_entry">
+                        <div className="form_entry , input__container">
                             <label for="message">Message</label>
                             <input type="text" placeholder="Send me a message and I'll reply you as soon as possible..." id="message" className="contact__input" required />
                         </div>
-                        <div>
+                        <div id="checkbox" className="input__container">
                         <input type='checkbox' required ></input>You agree to providing your data to Emmanuel who may contact you.
                         </div>
-                        <div>
-                        <button type="submit">Send Message</button>
+                        <div className="input__container">
+                        <button type="submit" id="btn__submit">Send Message</button>
                         </div>
                     </div>
                 </form>
